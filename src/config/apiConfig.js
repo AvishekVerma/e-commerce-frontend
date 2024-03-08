ba763c739b34bb00a0f9af01b3859ab5f4b2c2d7
Avishek Verma <avishekverma298@gmail.com>
2024-03-08 08:57:08 +0530

    Completed action and reducers for product, order and cart

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -5,14 +5,13 @@ const LOCALHOST='http://localhost:9095'
 
 export const API_BASE_URL = LOCALHOST
 
-// const api = axios.create({
-//   baseURL: API_BASE_URL,
-// });
+const jwt = localStorage.getItem("jwt")
 
-// const token = localStorage.getItem('jwt');
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers:{
+    "Authorization":`Bearer ${jwt}`,
+    "content-Type":"application/json"
+  }
+});
 
-// api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
-// api.defaults.headers.post['Content-Type'] = 'application/json';
-
-// export default api;
